f2b58a4b9beb7b6be28fea37f03882220e45c6ba
kathrynozgun <kathryn.a.ozgun@gmail.com>
2021-03-19 15:12:49 -0500

    Population Demographics - clean

--- assess_population_demographics.py
+++ assess_population_demographics.py
@@ -17,96 +17,148 @@ import re
 from math import pi
 
 
-# reads excel into the dataframe
+# reads friend's movie watch sheet into the dataframe
+# friends were instructed to use a 'x' or 'X' to indicate they watched a movie
 movie_ratings = pd.read_csv('Top-1000-Movies-List-032021.csv')
-print( movie_ratings.columns)
+movie_ratings = movie_ratings.dropna(axis = 0, how = 'all')
 
-# Get rid of all of the rows with all nans
-# axis=0 means removing rows
-core_ratings = movie_ratings.dropna(axis = 0, how = 'all')
-core_ratings = core_ratings[:1001]  # keep the first 1000 rows
+# sort by the imbd code
+movie_ratings['ttcode'] = movie_ratings['URL'].str.replace('https://www.imdb.com/title/','')
+movie_ratings['ttcode'] = movie_ratings['ttcode'].str.replace('/','')
+movie_ratings = movie_ratings.sort_values('ttcode')
+movie_ratings = movie_ratings[:1000]  # keep the first 1000 rows
+
+
+# load in movie details obtained from the omdb api
+movie_details  = pd.read_pickle("./movie_details.pkl")
+movie_details = movie_details.sort_values(by=['imdbID'])
+movie_details = movie_details[:1000]  # keep the first 1000 rows
+
+# since the movie title varies between dataframes 
+# e.g. english/alt language, accents, etc, lets sort by the imdb ID
+movie_details = movie_details.set_index('imdbID')
+movie_ratings = movie_ratings.set_index('ttcode')
+
 
 # friend names to consider
 friend_idx = range(3,11)
+print('Assessing Population Demographics for ', movie_ratings.columns[friend_idx].tolist(), '\n')
+print('Movie Details: ', movie_details.columns.tolist(), '\n')
+
 
 #%%  Movie Totals 
 
-num_indvl_ratings = dict(1000-core_ratings.iloc[:,friend_idx].isna().sum())
+num_indvl_ratings = dict(1000-movie_ratings.iloc[:,friend_idx].isna().sum())
 
 # Show a total sum of the movies that people have seen
-ax = plt.subplot(111)
-ax.bar(num_indvl_ratings.keys(), num_indvl_ratings.values(), width=0.2, align='center')
-plt.title('Number of 1000 movies they have seen', fontsize=  16)
+fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(7, 5), dpi=300)
+ax.bar(num_indvl_ratings.keys(), num_indvl_ratings.values(), edgecolor=[0.3, 0.3, 0.3], width=0.2, align='center')
+plt.title('Number of Top 1000 Movies Watched', fontsize = 16)
+plt.savefig('barplot_movies_watched_by_friends.png')
+plt.show()
 
+#%% Movie Data Topics
 
+# Create an array that contains the different rating types
+genre_list = movie_ratings["Genres"].str.split(', ', expand=True).stack().unique()
 
-#%% Movie Genres
+# Create an array of all actors
+actor_list = []
+for actor in movie_details["Actors"].str.split(', ', expand=True).stack().unique():
+        if re.search(' ', actor): # Let's only look at actors with a first and last name to prevent errors
+            actor_list.append(actor)
+            
 
-# Create a dataframe that contains the different rating types
-genre_list = core_ratings["Genres"].str.split(', ', expand=True).stack().unique()
+#%% Compile Movie Data
 
-# Gather the genre data for each friend   
-genre_data = []
-genre_total = []
-for friend_name in core_ratings.columns[friend_idx]:
-    movies_watched = core_ratings[friend_name].str.match('X', flags=re.IGNORECASE)
+# Gather the data topics for each friend   
+genre_data = []; genre_total = []
+actor_data = []; actor_total = []
+decade_list = [] 
+
+for friend_name in movie_ratings.columns[friend_idx]:
+    
+    movies_watched = movie_ratings[friend_name].str.match('X', flags=re.IGNORECASE) 
     movies_watched = movies_watched.fillna(False)
+ 
+    decade_list.append(movie_ratings["Year"][movies_watched].tolist());
     
     gr_list = []
     for genre_type in genre_list:
-        specific_genre = core_ratings["Genres"].str.contains(genre_type)
+        specific_genre = movie_ratings["Genres"].str.contains(genre_type)
         ismatch = numpy.logical_and(movies_watched, specific_genre)
         gr_list.append(sum(ismatch))
-        if friend_name == core_ratings.columns[3]:
+        if friend_name == movie_ratings.columns[friend_idx[0]]:
             genre_total.append(sum(specific_genre))
-        
     genre_data.append(gr_list)
+    
+    ac_list = []
+    for actor_name in actor_list:
+        specific_actor = movie_details["Actors"].str.contains(actor_name)
+        # suppose we are interested in movies from the USA specifically
+        specific_country = movie_details["Country"].str.contains('USA')
+        ismatch = numpy.logical_and(movies_watched, specific_actor)
+        ismatch = numpy.logical_and(ismatch, specific_country)
+        ac_list.append(sum(ismatch))
+        if friend_name == movie_ratings.columns[friend_idx[0]]:
+            actor_total.append(sum(numpy.logical_and(specific_actor, specific_country)))        
+    actor_data.append(ac_list)
+
 genre_data.append(genre_total)
+actor_data.append(actor_total)  
+
 
 # Create a dataframe with our data
-genre_df = pd.DataFrame(genre_data, columns=genre_list, index=numpy.insert(core_ratings.columns[friend_idx], len(friend_idx),'Total'))
+genre_df = pd.DataFrame(genre_data, columns=genre_list, index=numpy.insert(movie_ratings.columns[friend_idx], len(friend_idx),'Total'))
+actor_df = pd.DataFrame(actor_data, columns=actor_list, index=numpy.insert(movie_ratings.columns[friend_idx], len(friend_idx),'Total'))
 
-genre_total_df = pd.DataFrame([genre_total], columns=genre_list)
 
-# Create a copy of the genre data without genres none of us watch
+#%%% Genres
+
+# Create a copy of the genre data to create a subset of genres that are important to us
 genre_df_subset = genre_df.copy('deep=True')
-genre_total_subset = genre_total_df.copy('deep=True')
 for genre_type in genre_df.columns[1:]:
     if(genre_df[genre_type].sum()==0):
         del genre_df_subset[genre_type]
-        del genre_total_subset[genre_type]
         
 # Manually remove niche genres we don't really care about
-del genre_df_subset['War'];  del genre_total_subset['War']
-del genre_df_subset['Film-Noir'];  del genre_total_subset['Film-Noir']
-del genre_df_subset['Western'];  del genre_total_subset['Western']
-del genre_df_subset['Sport'];  del genre_total_subset['Sport']
-del genre_df_subset['Musical'];  del genre_total_subset['Musical']
-del genre_df_subset['Drama'];  del genre_total_subset['Drama']  #basically every movie is a drama
+del genre_df_subset['War'];  
+del genre_df_subset['Film-Noir']; 
+del genre_df_subset['Western'];  
+del genre_df_subset['Sport'];  
+del genre_df_subset['Musical']; 
+del genre_df_subset['History']; 
+del genre_df_subset['Biography']; 
 
-N = genre_df_subset.shape[1] # number of genres
-angles = [n / float(N) * 2 * pi for n in range(N)]  # intialize angles
+# Nearly every movie is a drama, so this genre isn't that important to us
+del genre_df_subset['Drama'];  
+
+
+# Generate a radar plot
+N_angles = genre_df_subset.shape[1] # number of genres
+angles = [n / float(N_angles) * 2 * pi for n in range(N_angles)]  # intialize angles
 angles += angles[:1]
 
-fig, axes = plt.subplots(nrows=2, ncols=4, figsize=(12, 7), dpi=150)
-fig.suptitle('Percent of Movies in Top 1000 Watched By Genre', fontsize = 16)
+fig, axes = plt.subplots(nrows=2, ncols=4, figsize=(12, 7), dpi=300)
+fig.suptitle('Number Movies in Top 1000 Watched By Genre', fontsize = 16)
 fig.tight_layout() 
 
 for fr_idx in range( genre_df_subset.shape[0]-1):
     
     values = genre_df_subset.loc[genre_df_subset.index[fr_idx]].values.flatten().tolist()
     # lets get a percent of the total number 
-   # values = [ values[idx] / genre_total_subset.loc[0].values.tolist()[idx] * 100 for idx in range(len(values)) ]
+    #values = [ values[idx] / genre_df_subset.loc[genre_df_subset.index[-1]].values.tolist()[idx] * 100 for idx in range(len(values)) ]
     values += values[:1] # repeat last value  
     
     ax = plt.subplot(2,4,fr_idx+1, polar=True)
      
     # Draw one axe per variable + add labels
-    plt.xticks(angles[:-1], genre_df_subset.columns[:], color='grey', size=9)
-     
+    plt.xticks(angles[:-1], genre_df_subset.columns[:], color=[0.3,0.3,0.3], size=9)
+    ax.tick_params(axis='x', which='major', pad=12)
+
     # Draw ylabels
     ax.set_rlabel_position(0)
-    plt.yticks([10,30,50], ["10","30","50"], color="grey", size=10);  plt.ylim(0,55)
+    plt.yticks([20,40,60], ["20","40","60"], color=[0.3,0.3,0.3], size=10);  plt.ylim(0,65)
     plt.title(genre_df_subset.index[fr_idx])
      
     # Plot data
@@ -115,58 +167,20 @@ for fr_idx in range( genre_df_subset.shape[0]-1):
     
     # Show the graph
 plt.subplots_adjust( wspace=0.7, hspace=0.3)
+plt.savefig('popularity_by_genre.png')
 plt.show()
-    
+
+
 
 #%% Actor/Actress Preferences
 
-movie_details_df  = pd.read_pickle("./movie_details.pkl")
-movie_details_df = movie_details_df.sort_values(by=['imdbID'])
-
-core_ratings['ttcode'] = core_ratings['URL'].str.replace('https://www.imdb.com/title/','')
-core_ratings['ttcode'] = core_ratings['ttcode'].str.replace('/','')
-core_ratings = core_ratings.sort_values('ttcode')
-
-movie_details_df = movie_details_df.set_index('imdbID')
-core_ratings = core_ratings.set_index('ttcode')
-
-
-actor_list = []
-for actor in movie_details_df["Actors"].str.split(', ', expand=True).stack().unique():
-        if re.search(' ', actor):
-            actor_list.append(actor)
-
-
-# Gather the actor data for each friend   
-actor_data = []
-actor_total = []
-for friend_name in core_ratings.columns[friend_idx]:
-    movies_watched = core_ratings[friend_name].str.match('X', flags=re.IGNORECASE)
-    movies_watched = movies_watched.fillna(False)
-    
-    ac_list = []
-    for actor_name in actor_list:
-        specific_actor = movie_details_df["Actors"].str.contains(actor_name)
-        specific_country = movie_details_df["Country"].str.contains('USA')
-        ismatch = numpy.logical_and(movies_watched, specific_actor)
-        ismatch = numpy.logical_and(ismatch, specific_country)
-        ac_list.append(sum(ismatch))
-        if friend_name == core_ratings.columns[3]:
-            actor_total.append(sum(numpy.logical_and(specific_actor, specific_country)))
-        
-    actor_data.append(ac_list)
-actor_data.append(actor_total)
-
-
-
 # Create a dataframe with our data
-actor_df = pd.DataFrame(actor_data, columns=actor_list, index=numpy.insert(core_ratings.columns[friend_idx], len(friend_idx),'Total'))
-
 actor_df.head()
 actors_sorted = actor_df.sort_values(by = 'Total', axis = 1, ascending = False) 
 actors_sorted.head(10)
 
-fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(20, 6), dpi=150,)
+fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(20, 6), dpi=300,)
+# Lets look at the top 50 Actors and Actresses 
 actors_sorted_small = actors_sorted.iloc[:, 0:50]
 
 plt.pcolor(actors_sorted_small)
@@ -175,37 +189,30 @@ plt.xticks(numpy.arange(0.5, len(actors_sorted_small.columns), 1), actors_sorted
 plt.colorbar()
 plt.xlabel('Actor')
 plt.ylabel('Friend')
-plt.title('Frequency of US Movies Starring Actor/Actresses', fontsize=16)
+plt.title('Frequency of US Movies Starring Top 50 Actor/Actresses', fontsize=16)
+plt.savefig('popularity_by_actor.png')
 plt.show()
 
 
-#%% Lets look at movie decades
 
-decade_list = [] 
-actor_data = []
-actor_total = []
-for friend_name in core_ratings.columns[friend_idx]:
-    movies_watched = core_ratings[friend_name].str.match('X', flags=re.IGNORECASE)
-    movies_watched = movies_watched.fillna(False)
-    
-    decade_list.append(core_ratings["Year"][movies_watched].tolist());
-    
-    
+#%% Lets look at the popularity of  movie decades between 1900 and 2015
+
 bins = numpy.linspace(1900,2015, 24)
 str_ticks = [ str(round(bin)) for bin in bins if bin%20==0]
 ticks = [ round(bin) for bin in bins if bin%20==0]
 
-fig, axes = plt.subplots(nrows=2, ncols=4, figsize=(16, 8), dpi=150)
+fig, axes = plt.subplots(nrows=2, ncols=4, figsize=(16, 8), dpi=300)
 fig.suptitle('Movie decade preference', fontsize = 16)
 fig.tight_layout()
-for fr_idx in range(len(core_ratings.columns[friend_idx])):
+for fr_idx in range(len(movie_ratings.columns[friend_idx])):
     ax = plt.subplot(2,4,fr_idx+1)
-    plt.hist(decade_list[fr_idx], bins, color='b', alpha=0.5, density=True, stacked=True)
-    plt.title(core_ratings.columns[friend_idx][fr_idx])
+    plt.hist(decade_list[fr_idx], bins, edgecolor=[0.3, 0.3, 0.3], density=True, stacked=True)
+    plt.title(movie_ratings.columns[friend_idx][fr_idx])
     plt.xticks( ticks, str_ticks, color="grey", size=10);  plt.xlim(1900, 2020)
     plt.ylim(0, 0.08)
  
 plt.subplots_adjust( wspace=0.3, hspace=0.5)
+plt.savefig('popularity_by_decade.png')
 plt.show()
 
 
